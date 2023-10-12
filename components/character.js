24c3f9b352573237ad1a67301eff83f13a4a668f
majikyouser <sahiagb@gmail.com>
2023-10-12 11:56:04 -0400

    setup character general class

---
+++ components/character.js
@@ -0,0 +1,40 @@
+export class Character {
+    constructor(name, speed, jumpHeight, jumpSpeed, x, y, hitboxes, sprite) {
+        this.name = name;
+        this.speed = speed;
+        this.jumpHeight = jumpHeight;
+        this.jumpSpeed = jumpSpeed;
+        this.x = x;
+        this.y = y;
+        this.hitboxes = hitboxes;
+        this.sprite = sprite;
+    }
+
+    move() {
+        // TODO
+    }
+
+    jump() {
+        // TODO
+    }
+
+    basicAttack() {
+        // TODO
+    }
+
+    specialAttack() {
+        // TODO
+    }
+
+    block() {
+        // TODO
+    }
+
+    takeDamage() {
+        // TODO
+    }
+
+    die() {
+        // TODO
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,15 @@
+import character from "./components/character.js";
+
+function preload() {
+
+}
+
+function setup() {
+  createCanvas(500, 500);
+  background(0);
+}
+
+
+function draw() {
+
+}
